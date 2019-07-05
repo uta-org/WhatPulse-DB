fcf661ef884d4178fe2418ba665e51d3f4afe6bc
Ikillnukes <ikillnukes1@gmail.com>
2019-07-05 06:41:39 +0200

    Added client

---
+++ src/WhatPulseDB.Crawler/WhatPulseDB/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatPulseDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
